fbf999718517d5a8797df3df6c6191a74f7a48cf
zhiy930 <zhiy930@gmail.com>
2026-07-24 17:07:33 +0800

    Allow Railway frontend host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
+  preview: {
+    allowedHosts : ['outstanding-surprise-production-f135.up.railway.app']
+  }
 })
