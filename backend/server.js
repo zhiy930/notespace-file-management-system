3c35f85c17f5729380c22ff8f951033a3a441e71
zhiy930 <zhiy930@gmail.com>
2026-07-24 16:16:53 +0800

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const db = require("./database");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 // ==========================================
